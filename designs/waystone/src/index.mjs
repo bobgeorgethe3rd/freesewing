31f9cc66f37eb32e4ea5664ac342bd376c6642bc
bobgeorgethe3rd <bobgeorgethe3rd@googlemail.com>
2022-10-10 15:24:43 +0000

    Create Waystone Desgin

---
+++ designs/waystone/src/index.mjs
@@ -0,0 +1,13 @@
+import { Design } from '@freesewing/core'
+import { data } from '../data.mjs'
+// Parts
+import { box } from './box.mjs'
+
+// Create new design
+const Waystone = new Design({
+  data,
+  parts: [box],
+})
+
+// Named exports
+export { box, Waystone }
--- designs/waystone/tests/shared.test.mjs
+++ designs/waystone/tests/shared.test.mjs
@@ -1,5 +1,5 @@
 // This file is auto-generated | Any changes you make will be overwritten.
-import { Examples } from '../src/index.mjs'
+import { Waystone } from '../src/index.mjs'
 
 // Shared tests
 import { testPatternConfig } from '../../../tests/designs/config.mjs'
@@ -7,10 +7,10 @@ import { testPatternDrafting } from '../../../tests/designs/drafting.mjs'
 import { testPatternSampling } from '../../../tests/designs/sampling.mjs'
 
 // Test config
-testPatternConfig(Examples)
+testPatternConfig(Waystone)
 
 // Test drafting - Change the second parameter to `true` to log errors
-testPatternDrafting(Examples, false)
+testPatternDrafting(Waystone, false)
 
 // Test sampling - Change the second parameter to `true` to log errors
-testPatternSampling(Examples, false)
+testPatternSampling(Waystone, false)
