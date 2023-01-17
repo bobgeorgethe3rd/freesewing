793b9a08014f1c29db3fe6278bbc3373449f6cc7
bobgeorgethe3rd <bobgeorgethe3rd@googlemail.com>
2023-01-17 19:20:18 +0000

    Adding Peach

---
+++ designs/peach/src/frontBase.mjs
@@ -0,0 +1,116 @@
+import { front as frontDaisy } from '@freesewing/daisy'
+
+export const frontBase = {
+  name: 'frontBase',
+  from: frontDaisy,
+  hideDependencies: true,
+  measurements: ['shoulderToWrist', 'wrist'],
+  options: {
+    //Constamt
+    bustDartLength: 1,
+    bustDartCurve: 1,
+    waistDartLength: 1,
+    //Darts
+    bustDartPlacement: {
+      dflt: 'armhole',
+      list: ['armhole', 'shoulder'],
+      menu: 'darts',
+    },
+  },
+  draft: ({
+    store,
+    sa,
+    Point,
+    points,
+    Path,
+    paths,
+    options,
+    complete,
+    paperless,
+    macro,
+    utils,
+    measurements,
+    part,
+    snippets,
+  }) => {
+    //removing paths and snippets not required from Bella
+    for (let i in paths) delete paths[i]
+    for (let i in snippets) delete snippets[i]
+    //removing macros not required from Bella
+    macro('title', false)
+    macro('scalebox', false)
+    //let's begin
+    points.bustDartCpBottom = points.bust
+      .shiftFractionTowards(points.bustDartBottom, 2 / 3)
+      .rotate(5, points.bust)
+
+    points.waistDartMiddleCp = points.bust.shiftFractionTowards(points.waistDartHem, 0.25)
+    points.bustDartCpMiddle = points.waistDartMiddleCp.rotate(180, points.bust)
+    if (options.bustDartPlacement == 'armhole') {
+      points.waistDartLeftCp = utils.beamsIntersect(
+        points.bustDartCpTop,
+        points.bustDartCpMiddle,
+        points.waistDartLeft,
+        points.waistDartLeftCp
+      )
+    }
+
+    //guides
+    const drawBellaGuide = () => {
+      if (options.bustDartPlacement == 'armhole')
+        return new Path()
+          .move(points.cfHem)
+          .line(points.waistDartLeft)
+          .curve_(points.waistDartLeftCp, points.bust)
+          ._curve(points.waistDartRightCp, points.waistDartRight)
+          .line(points.sideHem)
+          .line(points.armhole)
+          .curve(points.armholeCp2, points.armholePitchCp1, points.bustDartBottom)
+          ._curve(points.bustDartCpBottom, points.bust)
+          .curve_(points.bustDartCpTop, points.bustDartTop)
+          .curve_(points.armholePitchCp2, points.shoulder)
+          .line(points.hps)
+          .curve(points.hpsCp2, points.cfNeckCp1, points.cfNeck)
+          .line(points.cfHem)
+      else
+        return new Path()
+          .move(points.cfWaist)
+          .line(points.waistDartLeft)
+          .curve_(points.waistDartLeftCp, points.waistDartTip)
+          ._curve(points.waistDartRightCp, points.waistDartRight)
+          .line(points.sideWaist)
+          .line(points.armhole)
+          .curve(points.armholeCp2, points.armholePitchCp1, points.armholePitch)
+          .curve_(points.armholePitchCp2, points.shoulder)
+          .line(points.bustDartBottom)
+          ._curve(points.bustDartCpBottom, points.bust)
+          .curve_(points.bustDartCpTop, points.bustDartTop)
+          .line(points.hps)
+          .curve(points.hpsCp2, points.cfNeckCp1, points.cfNeck)
+          .line(points.cfWaist)
+    }
+
+    paths.bellaGuide = drawBellaGuide().close().attr('class', 'various lashed')
+
+    const drawFrontSeam = () => {
+      if (options.bustDartPlacement == 'armhole')
+        return new Path()
+          .move(points.waistDartLeft)
+          .curve(points.waistDartLeftCp, points.bustDartCpTop, points.bustDartTop)
+      else
+        return new Path()
+          .move(points.waistDartLeft)
+          .curve(points.waistDartLeftCp, points.waistDartMiddleCp, points.bust)
+          .curve(points.bustDartCpMiddle, points.bustDartCpTop, points.bustDartTop)
+    }
+
+    paths.frontSeam = drawFrontSeam()
+
+    paths.sideFrontSeam = new Path()
+      .move(points.bustDartBottom)
+      .curve(points.bustDartCpBottom, points.bustDartCpMiddle, points.bust)
+      .curve(points.waistDartMiddleCp, points.waistDartRightCp, points.waistDartRight)
+
+    return part
+  },
+}
