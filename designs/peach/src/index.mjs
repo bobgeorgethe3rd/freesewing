d042b7b3a6b7f96405093ddb833956681b016831
bobgeorgethe3rd <bobgeorgethe3rd@googlemail.com>
2023-01-18 15:14:02 +0000

    Separating back

--- designs/peach/src/index.mjs
+++ designs/peach/src/index.mjs
@@ -7,12 +7,14 @@ import { frontBase } from './frontBase.mjs'
 import { backBase } from './backBase.mjs'
 import { front } from './front.mjs'
 import { sideFront } from './sideFront.mjs'
+import { back } from './back.mjs'
+import { sideBack } from './sideBack.mjs'
 
 // Create new design
 const Peach = new Design({
   data,
-  parts: [frontBase, backBase, front, sideFront],
+  parts: [frontBase, backBase, front, sideFront, back, sideBack],
 })
 
 // Named exports
-export { frontBase, backBase, front, sideFront, Peach }
+export { frontBase, backBase, front, sideFront, back, sideBack, Peach }
