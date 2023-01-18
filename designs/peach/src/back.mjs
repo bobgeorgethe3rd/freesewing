e0142db682811af299d8ff883a49a83d9237396d
bobgeorgethe3rd <bobgeorgethe3rd@googlemail.com>
2023-01-18 15:14:02 +0000

    Separating back

---
+++ designs/peach/src/back.mjs
@@ -0,0 +1,81 @@
+import { backBase } from './backBase'
+
+export const back = {
+  name: 'back',
+  from: backBase,
+  hideDependencies: true,
+  draft: ({
+    store,
+    sa,
+    Point,
+    points,
+    Path,
+    paths,
+    options,
+    complete,
+    paperless,
+    macro,
+    utils,
+    measurements,
+    part,
+    snippets,
+    Snippet,
+  }) => {
+    //removing paths and snippets not required from Bella
+    for (let i in paths) delete paths[i]
+    for (let i in snippets) delete snippets[i]
+    //removing macros not required from Bella
+    macro('title', false)
+    macro('scalebox', false)
+
+    //seam paths
+
+    const drawSeamBase = () => {
+      if (options.bustDartPlacement == 'armhole')
+        return new Path()
+          .move(points.dartTip)
+          .curve_(points.backCp1, points.armholePitch)
+          .curve_(points.armholePitchCp2, points.shoulder)
+      else return new Path().move(points.dartTip).curve_(points.backCp1, points.shoulderSplit)
+    }
+
+    paths.seam = new Path()
+      .move(points.waistCenter)
+      .line(points.dartBottomLeft)
+      .curve(points.dartLeftCp, points.dartMiddleCp, points.dartTip)
+      .join(drawSeamBase())
+      .line(points.hps)
+      ._curve(points.cbNeckCp1, points.cbNeck)
+      .curve_(points.cbNeckCp2, points.waistCenter)
+      .close()
+
+    if (complete) {
+      //grainline
+      points.grainlineFrom = points.cbNeck.shiftFractionTowards(points.cbNeckCp1, 1 / 3)
+      points.grainlineTo = new Point(points.grainlineFrom.x, points.cbWaist.y)
+      macro('grainline', {
+        from: points.grainlineFrom,
+        to: points.grainlineTo,
+      })
+      //notches
+      snippets.dartTip = new Snippet('notch', points.dartTip)
+      //title
+      points.title = new Point(points.cbNeckCp1.x * (3 / 4), points.cbWaist.y / 2)
+      macro('title', {
+        at: points.title,
+        nr: '3',
+        title: 'Back',
+      })
+      //scalebox
+      points.scalebox = new Point(points.hps.x, points.cbArmhole.y / 3)
+      macro('scalebox', {
+        at: points.scalebox,
+      })
+      if (sa) {
+        paths.sa = paths.seam.offset(sa).close().attr('class', 'fabric sa')
+      }
+    }
+
+    return part
+  },
+}
---
+++ designs/peach/src/sideBack.mjs
@@ -0,0 +1,77 @@
+import { backBase } from './backBase'
+
+export const sideBack = {
+  name: 'sideBack',
+  from: backBase,
+  hideDependencies: true,
+  draft: ({
+    store,
+    sa,
+    Point,
+    points,
+    Path,
+    paths,
+    options,
+    complete,
+    paperless,
+    macro,
+    utils,
+    measurements,
+    part,
+    snippets,
+    Snippet,
+  }) => {
+    //removing paths and snippets not required from Bella
+    for (let i in paths) delete paths[i]
+    for (let i in snippets) delete snippets[i]
+    //removing macros not required from Bella
+    macro('title', false)
+    macro('scalebox', false)
+
+    //seam paths
+
+    const drawSeamBase = () => {
+      if (options.bustDartPlacement == 'armhole')
+        return new Path().move(points.armholePitch)._curve(points.backCp1, points.dartTip)
+      else
+        return new Path()
+          .move(points.armholePitch)
+          .curve_(points.armholePitchCp2, points.shoulder)
+          .line(points.shoulderSplit)
+          ._curve(points.backCp1, points.dartTip)
+    }
+
+    paths.seam = new Path()
+      .move(points.dartTip)
+      .curve(points.dartMiddleCp, points.dartRightCp, points.dartBottomRight)
+      .line(points.waistSide)
+      .curve_(points.waistSideCp2, points.armhole)
+      .curve(points.armholeCp2, points.armholePitchCp1, points.armholePitch)
+      .join(drawSeamBase())
+      .close()
+
+    if (complete) {
+      //grainline
+      points.grainlineFrom = points.armholeCp2
+      points.grainlineTo = new Point(points.armholeCp2.x, points.waistSide.y)
+      macro('grainline', {
+        from: points.grainlineFrom,
+        to: points.grainlineTo,
+      })
+      //notches
+      snippets.dartTip = new Snippet('notch', points.dartTip)
+      //title
+      points.title = new Point(points.dartRightCp.x * 1.1, points.dartRightCp.y)
+      macro('title', {
+        at: points.title,
+        nr: '4',
+        title: 'Side Back',
+      })
+      if (sa) {
+        paths.sa = paths.seam.offset(sa).close().attr('class', 'fabric sa')
+      }
+    }
+
+    return part
+  },
+}
