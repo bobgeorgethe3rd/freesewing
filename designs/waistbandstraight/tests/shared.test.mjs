c34de6e5008b417fb24568c860906687b90635d7
bobgeorgethe3rd <bobgeorgethe3rd@googlemail.com>
2023-03-29 13:01:54 +0100

    Setup waistband straight

---
+++ designs/waistbandstraight/tests/shared.test.mjs
@@ -0,0 +1,16 @@
+// This file is auto-generated | Any changes you make will be overwritten.
+import { Waistbandstraight } from '../src/index.mjs'
+
+// Shared tests
+import { testPatternConfig } from '../../../tests/designs/config.mjs'
+import { testPatternDrafting } from '../../../tests/designs/drafting.mjs'
+import { testPatternSampling } from '../../../tests/designs/sampling.mjs'
+
+// Test config
+testPatternConfig(Waistbandstraight)
+
+// Test drafting - Change the second parameter to `true` to log errors
+testPatternDrafting(Waistbandstraight, false)
+
+// Test sampling - Change the second parameter to `true` to log errors
+testPatternSampling(Waistbandstraight, false)
