fc28a0302b8cb8c9e6b74b0d59d3ff95d39f11bb
bobgeorgethe3rd <bobgeorgethe3rd@googlemail.com>
2023-04-09 17:08:17 +0100

    Box pleat Pocket setup

---
+++ designs/boxpleatpocket/tests/shared.test.mjs
@@ -0,0 +1,16 @@
+// This file is auto-generated | Any changes you make will be overwritten.
+import { Boxpleatpocket } from '../src/index.mjs'
+
+// Shared tests
+import { testPatternConfig } from '../../../tests/designs/config.mjs'
+import { testPatternDrafting } from '../../../tests/designs/drafting.mjs'
+import { testPatternSampling } from '../../../tests/designs/sampling.mjs'
+
+// Test config
+testPatternConfig(Boxpleatpocket)
+
+// Test drafting - Change the second parameter to `true` to log errors
+testPatternDrafting(Boxpleatpocket, false)
+
+// Test sampling - Change the second parameter to `true` to log errors
+testPatternSampling(Boxpleatpocket, false)
