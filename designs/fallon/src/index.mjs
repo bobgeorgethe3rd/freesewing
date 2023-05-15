f3f1eea9e2d5df58f61d9e21c3f6b9ff23e13ce6
bobgeorgethe3rd <bobgeorgethe3rd@googlemail.com>
2023-05-15 19:03:11 +0100

    Add Pokcets & Placket from Wanda

--- designs/fallon/src/index.mjs
+++ designs/fallon/src/index.mjs
@@ -5,11 +5,18 @@ import { data } from '../data.mjs'
 // Parts
 import { skirtBase } from './skirtBase.mjs'
 
+// Inherited Parts
+import { placket } from '@freesewing/wanda'
+import { inseamPocket } from '@freesewing/wanda'
+import { boxPleatPocket } from '@freesewing/wanda'
+import { pearPocket } from '@freesewing/wanda'
+import { watchPocket } from '@freesewing/wanda'
+
 // Create new design
 const Fallon = new Design({
   data,
-  parts: [skirtBase],
+  parts: [skirtBase, placket, inseamPocket, boxPleatPocket, pearPocket],
 })
 
 // Named exports
-export { skirtBase, Fallon }
+export { skirtBase, placket, inseamPocket, boxPleatPocket, pearPocket, Fallon }
