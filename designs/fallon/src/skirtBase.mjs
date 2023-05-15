0cbc763df4dddd34a3505c68b8b0d9925f95f835
bobgeorgethe3rd <bobgeorgethe3rd@googlemail.com>
2023-05-15 14:36:00 +0100

    Fallon Setup

---
+++ designs/fallon/src/skirtBase.mjs
@@ -0,0 +1,45 @@
+import { pluginBundle } from '@freesewing/plugin-bundle'
+import { skirtBase as wandaSkirtBase } from '@freesewing/wanda'
+
+export const skirtBase = {
+  name: 'fallon.skirtBase',
+  from: wandaSkirtBase,
+  hide: {
+    from: true,
+  },
+  options: {
+    //Constants
+    fullDress: true, //altered frot Fallon
+    umbrellaFullness: 0,
+    umbrellaExtenstion: 0,
+    pleats: false,
+    //Style
+    sidePanelFullness: { pct: 50, min: 50, max: 75, menu: 'style' },
+  },
+  plugins: [pluginBundle],
+  draft: ({
+    store,
+    sa,
+    Point,
+    points,
+    Path,
+    paths,
+    options,
+    complete,
+    paperless,
+    macro,
+    utils,
+    measurements,
+    part,
+    snippets,
+    Snippet,
+    absoluteOptions,
+    log,
+  }) => {
+    //removing paths
+    // for (let i in paths) delete paths[i]
+    //let's begin
+
+    return part
+  },
+}
