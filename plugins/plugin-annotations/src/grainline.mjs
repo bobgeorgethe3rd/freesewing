2b5353e210b59665beab5e1573a2681b64d2a42f
bobgeorgethe3rd <bobgeorgethe3rd@googlemail.com>
2023-05-28 17:31:11 +0000

    Adding the easy annotation ones

--- plugins/plugin-annotations/src/grainline.mjs
+++ plugins/plugin-annotations/src/grainline.mjs
@@ -1,8 +1,8 @@
 const markers = `
-<marker orient="auto" refY="4.0" refX="10.0" id="grainlineFrom" style="overflow:visible;" markerWidth="12" markerHeight="8">
+<marker orient="auto" refY="4.0" refX="0.0" id="grainlineFrom" style="overflow:visible;" markerWidth="12" markerHeight="8">
 	<path class="note fill-note" d="M 0,4 L 12,0 C 10,2 10,6  12,8 z" />
 </marker>
-<marker orient="auto" refY="4.0" refX="2.0" id="grainlineTo" style="overflow:visible;" markerWidth="12" markerHeight="8">
+<marker orient="auto" refY="4.0" refX="12.0" id="grainlineTo" style="overflow:visible;" markerWidth="12" markerHeight="8">
 	<path class="note fill-note" d="M 12,4 L 0,0 C 2,2 2,6  0,8 z" />
 </marker>`
 
@@ -16,12 +16,25 @@ export const grainlineHooks = {
     },
   ],
 }
+
 export const grainlineMacros = {
   grainline: function (so = {}, { points, paths, Path, complete, setGrain }) {
+    let prefix
+    if (so.id) {
+      prefix = '_' + so.id
+    } else {
+      prefix = ''
+    }
+
+    const id = prefix + '_grainline'
+
     if (so === false) {
-      delete points.grainlineFrom
-      delete points.grainlineTo
-      delete paths.grainline
+      for (const pointName in points) {
+        if (pointName.match('_grainline')) delete points[pointName]
+      }
+      for (const pathName in paths) {
+        if (pathName.match('_grainline')) delete paths[pathName]
+      }
       setGrain(90) // Restoring default
       return true
     }
@@ -34,11 +47,11 @@ export const grainlineMacros = {
       setGrain(so.from.angle(so.to))
     }
     if (complete) {
-      points.grainlineFrom = so.from.shiftFractionTowards(so.to, 0.05)
-      points.grainlineTo = so.to.shiftFractionTowards(so.from, 0.05)
-      paths.grainline = new Path()
-        .move(points.grainlineFrom)
-        .line(points.grainlineTo)
+      points[id + 'From'] = so.from.shiftFractionTowards(so.to, 0.05)
+      points[id + 'To'] = so.to.shiftFractionTowards(so.from, 0.05)
+      paths[id] = new Path()
+        .move(points[id + 'From'])
+        .line(points[id + 'To'])
         .attr('class', 'note')
         .attr('marker-start', 'url(#grainlineFrom)')
         .attr('marker-end', 'url(#grainlineTo)')
