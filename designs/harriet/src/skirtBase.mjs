3364cb4e948cb5f9b9a7490e56815d97540f7d98
bobgeorgethe3rd <bobgeorgethe3rd@googlemail.com>
2023-06-15 05:11:38 +0100

    beginning of front facing

--- designs/harriet/src/skirtBase.mjs
+++ designs/harriet/src/skirtBase.mjs
@@ -21,6 +21,9 @@ export const skirtBase = {
       menu: 'style',
     },
     sideSkirtFraction: { pct: 100, min: 0, max: 100, menu: 'style' },
+    //Construction
+    skirtFacings: { bool: true, menu: 'construction' },
+    skirtFacingWidth: { pct: 25, min: 10, max: 50, menu: 'construction' },
   },
   plugins: [pluginBundle],
   measurements: [...claudeSkirtBase.measurements],
@@ -28,13 +31,16 @@ export const skirtBase = {
   // after: skirtBase,
   draft: (sh) => {
     //set Render
-    const { macro, points, paths, Path, options, utils, store, complete, part } = sh
+    const { macro, points, Point, paths, Path, options, utils, store, complete, part } = sh
     claudeSkirtBase.draft(sh)
 
     //measures
     const skirtHighLength = store.get('skirtHighLength')
     const skirtLength = store.get('skirtLength')
     const skirtFraction = skirtHighLength / skirtLength
+    const skirtFacingWidth = skirtHighLength * options.skirtFacingWidth
+    const skirtFrontFullness = store.get('skirtFrontFullness')
+    const skirtBackFullness = store.get('skirtBackFullness')
 
     paths.backHemTest = new Path()
       .move(points.cbHem)
@@ -235,8 +241,152 @@ export const skirtBase = {
         points.backHemExSplit = points.backHemExtension
       }
     }
+    //facings
+    if (complete && options.skirtFacings) {
+      //frontHemFacing
+
+      // points.frontHemFacingMidAnchor = points.frontHemMid.shiftTowards(points.frontHemCp2, skirtFacingWidth).rotate(-90, points.frontHemMid)
+
+      // let skirtFacingWidthHL
+      // if (utils.linesIntersect(
+      // points.frontHemMid,
+      // points.frontHemFacingMidAnchor,
+      // points.frontHemCp4,
+      // points.waistFrontCp1
+      // )){
+      // skirtFacingWidthHL = skirtHighLength * 0.1
+      // }
+      // else {
+      // skirtFacingWidthHL = skirtFacingWidth
+      // }
+
+      // points.cfHemFacing = points.cfHem.shiftTowards(points.cfWaist, skirtFacingWidthHL)
+      // points.frontHemFacingMid = points.frontHemMid.shiftTowards(points.frontHemCp2, skirtFacingWidthHL).rotate(-90, points.frontHemMid)
+      // points.sideFrontHemFacing = points.sideFrontHem.shiftTowards(points.sideWaistFront, skirtFacingWidthHL)
+
+      // points.frontHemFacingCp1 = utils.beamsIntersect(
+      // points.cfHemFacing,
+      // points.cfWaist.rotate(-90, points.cfHemFacing),
+      // points.frontHemCp1,
+      // points.waistFrontCp4
+      // )
+      // points.frontHemFacingCp2 = points.frontHemCp2.shiftTowards(points.frontHemMid, skirtFacingWidthHL).rotate(90, points.frontHemCp2)
+      // points.frontHemFacingCp3 = points.frontHemCp3.shiftTowards(points.frontHemMid, skirtFacingWidthHL).rotate(-90, points.frontHemCp3)
+
+      // points.frontHemFacingCp4 = points.sideFrontHemFacing.shiftFractionTowards(utils.beamsIntersect(
+      // points.sideFrontHemFacing,
+      // points.sideWaistFront.rotate(90, points.sideFrontHemFacing),
+      // points.frontHemCp4,
+      // points.waistFrontCp1
+      // ), 0.85)
+
+      // paths.test = new Path()
+      // .move(points.cfHemFacing)
+      // .curve(points.frontHemFacingCp1, points.frontHemFacingCp2, points.frontHemFacingMid)
+      // .curve(points.frontHemFacingCp3, points.frontHemFacingCp4, points.sideFrontHemFacing)
+
+      const skirtFrontFacingAngle =
+        points.frontHemMid.angle(points.waistFrontMid) -
+        (points.frontHemMid.angle(points.frontHemCp2) - 90)
+      let skirtFrontFullnessMultiplier
+      if (skirtFrontFullness > 1) {
+        skirtFrontFullnessMultiplier = 1
+      } else {
+        skirtFrontFullnessMultiplier = skirtFrontFullness
+      }
+
+      points.cfHemFacing = points.cfHem.shiftTowards(points.cfWaist, skirtFacingWidth)
+      points.sideFrontHemFacing = points.sideFrontHem.shiftTowards(
+        points.sideWaistFront,
+        skirtFacingWidth
+      )
+
+      points.frontHemFacingMid = points.frontHemMid.shiftTowards(
+        points.waistFrontMid.rotate(
+          -skirtFrontFacingAngle * skirtFrontFullnessMultiplier,
+          points.frontHemMid
+        ),
+        skirtFacingWidth
+      )
+      points.frontHemFacingCp1 = utils.beamsIntersect(
+        points.frontHemCp1,
+        points.waistFrontCp4.rotate(
+          -skirtFrontFacingAngle * skirtFrontFullnessMultiplier,
+          points.frontHemCp1
+        ),
+        points.cfHemFacing,
+        points.cfWaist.rotate(-90, points.cfHemFacing)
+      )
+
+      points.frontHemFacingCp2 = utils.beamsIntersect(
+        points.frontHemCp2,
+        points.waistFrontCp3.rotate(
+          -skirtFrontFacingAngle * skirtFrontFullnessMultiplier,
+          points.frontHemCp2
+        ),
+        points.frontHemFacingMid,
+        points.frontHemMid.rotate(-90, points.frontHemFacingMid)
+      )
+
+      points.frontHemFacingCp3 = utils.beamsIntersect(
+        points.frontHemCp3,
+        points.waistFrontCp2.rotate(
+          -skirtFrontFacingAngle * skirtFrontFullnessMultiplier,
+          points.frontHemCp2
+        ),
+        points.frontHemFacingMid,
+        points.frontHemMid.rotate(90, points.frontHemFacingMid)
+      )
+
+      points.frontHemFacingCp4 = utils.beamsIntersect(
+        points.frontHemCp4,
+        points.waistFrontCp1.rotate(
+          -skirtFrontFacingAngle * skirtFrontFullnessMultiplier,
+          points.frontHemCp1
+        ),
+        points.sideFrontHemFacing,
+        points.sideWaistFront.rotate(90, points.sideFrontHemFacing)
+      )
+
+      if (skirtFrontFullness < 1) {
+        const skirtFrontFacingCpFraction =
+          points.frontHemFacingCp3.dist(points.frontHemFacingMid) /
+          points.frontHemFacingCp3.dist(points.frontHemFacingCp2)
+        points.frontHemFacingCp2 = points.frontHemFacingCp2.rotate(
+          (1 - skirtFrontFullness) * -skirtFrontFacingAngle,
+          points.frontHemFacingCp3
+        )
+        points.frontHemFacingMid = points.frontHemFacingCp3.shiftFractionTowards(
+          points.frontHemFacingCp2,
+          skirtFrontFacingCpFraction
+        )
+      }
+
+      paths.helper = new Path()
+        .move(points.frontHemCp3)
+        .line(points.waistFrontCp2)
+        .line(points.waistFrontCp3)
+        .line(points.frontHemCp2)
+
+      paths.frontHemFacingHelper = new Path()
+        .move(points.cfHemFacing)
+        .line(points.frontHemFacingCp1)
+        .line(points.frontHemFacingMid)
+        .line(points.frontHemFacingCp4)
+        .line(points.sideFrontHemFacing)
+        .line(points.frontHemFacingCp4)
+        .line(points.frontHemFacingCp1)
+        .attr('class', 'interfacing lashed')
+
+      paths.frontHemFacing = new Path()
+        .move(points.cfHemFacing)
+        .curve(points.frontHemFacingCp1, points.frontHemFacingCp2, points.frontHemFacingMid)
+        .curve(points.frontHemFacingCp3, points.frontHemFacingCp4, points.sideFrontHemFacing)
+      // .attr('class', 'interfacing')
+    }
     //stores
     store.set('skirtLength', points.sideWaistFront.dist(points.sideFrontHem))
+    store.set('skirtFacingWidth', skirtFacingWidth)
     //guides
     if (points.frontHemExtension) {
       paths.sideseamFront = new Path()
@@ -296,6 +446,9 @@ export const skirtBase = {
       .move(points.frontHemMid)
       .line(points.backHemMid)
       .attr('class', 'interfacing lashed')
+
+    paths.test6 = paths.frontHem.offset(-skirtFacingWidth).attr('class', 'interfacing lashed')
+    paths.test7 = paths.backHem.offset(-skirtFacingWidth).attr('class', 'canvas')
 
     return part
   },
