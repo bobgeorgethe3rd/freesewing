bfd37261729508b729d719af6e1b15cd478d5b27
bobgeorgethe3rd <bobgeorgethe3rd@googlemail.com>
2023-11-02 02:15:10 +0000

    Add Sleeve

--- designs/siona/src/index.mjs
+++ designs/siona/src/index.mjs
@@ -5,7 +5,7 @@ import { i18n } from '../i18n/index.mjs'
 import { data } from '../data.mjs'
 // Parts
 import { dress } from './dress.mjs'
-
+import { sleeve } from './sleeve.mjs'
 import { pocket } from './pocket.mjs'
 import { belt } from './belt.mjs'
 import { beltLoops } from './beltLoops.mjs'
@@ -14,8 +14,8 @@ import { channel } from './channel.mjs'
 // Create new design
 const Siona = new Design({
   data,
-  parts: [dress, pocket, belt, beltLoops, channel],
+  parts: [dress, sleeve, pocket, belt, beltLoops, channel],
 })
 
 // Named exports
-export { dress, pocket, belt, beltLoops, channel, i18n, Siona }
+export { dress, sleeve, pocket, belt, beltLoops, channel, i18n, Siona }
---
+++ designs/siona/src/sleeve.mjs
@@ -0,0 +1,92 @@
+import { dress } from './dress.mjs'
+
+export const sleeve = {
+  name: 'siona.sleeve',
+  after: dress,
+  measurements: ['shoulderToElbow', 'shoulderToWrist'],
+  options: {
+    //Fit
+    wristEase: { pct: 40, min: 0, max: 50, menu: 'fit' },
+    //Style
+    sleeves: { bool: true, menu: 'style' },
+    sleeveLength: { pct: 100, min: 10, max: 100, menu: 'style' },
+    sleeveLengthBonus: { pct: 0, min: -20, max: 50, menu: 'style' },
+    //Construction
+    sleeveHemWidth: { pct: 2, min: 1, max: 3, menu: 'construction' },
+  },
+  draft: ({
+    store,
+    sa,
+    Point,
+    points,
+    Path,
+    paths,
+    options,
+    paperless,
+    macro,
+    measurements,
+    part,
+  }) => {
+    if (!options.sleeves) {
+      return part.hide()
+    }
+    //measurements
+    const maxWidth = store.get('upperArmWidth')
+    const bottomWidth =
+      maxWidth * (1 - options.sleeveLength) +
+      measurements.wrist * (1 + options.wristEase) * options.sleeveLength
+    let sleeveLength
+    if (options.sleeveLength < 0.5) {
+      sleeveLength = measurements.shoulderToElbow * options.sleeveLength * 2
+    } else {
+      sleeveLength =
+        measurements.shoulderToElbow * (-2 * options.sleeveLength + 2) +
+        measurements.shoulderToWrist * (2 * options.sleeveLength - 1)
+    }
+    sleeveLength = sleeveLength * (1 + options.sleeveLengthBonus)
+    //let's begin
+    points.topMid = new Point(0, 0)
+    points.topRight = points.topMid.shift(0, maxWidth / 2)
+    points.topLeft = points.topRight.flipX()
+    points.bottomMid = points.topMid.shift(-90, sleeveLength)
+    points.bottomRight = points.bottomMid.shift(0, bottomWidth / 2)
+    points.bottomLeft = points.bottomRight.flipX()
+
+    //paths
+    paths.hemBase = new Path().move(points.bottomLeft).line(points.bottomRight).hide()
+
+    paths.saBase = new Path()
+      .move(points.bottomRight)
+      .line(points.topRight)
+      .line(points.topLeft)
+      .line(points.bottomLeft)
+      .hide()
+
+    paths.seam = paths.hemBase.join(paths.saBase).close()
+
+    //details
+    //grainline
+    points.grainlineTo = points.bottomMid.shiftFractionTowards(points.bottomLeft, 0.5)
+    points.grainlineFrom = new Point(points.grainlineTo.x, points.topMid.y)
+    macro('grainline', {
+      from: points.grainlineFrom,
+      to: points.grainlineTo,
+    })
+    //title
+    points.title = points.topMid.shiftFractionTowards(points.bottomMid, 0.5)
+    macro('title', { at: points.title, nr: 2, title: 'sleeve', scale: 0.5 })
+
+    if (sa) {
+      paths.sa = paths.saBase
+        .offset(sa * options.sideSeamSaWidth * 100)
+        .join(paths.hemBase.offset(sa * options.sleeveHemWidth * 100))
+        .close()
+        .attr('class', 'fabric sa')
+    }
+
+    if (paperless) {
+    }
+
+    return part
+  },
+}
