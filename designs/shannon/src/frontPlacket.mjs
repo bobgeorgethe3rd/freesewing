fc792c186973f3f6d7607b39340ec4d276bcc777
bobgeorgethe3rd <bobgeorgethe3rd@googlemail.com>
2023-12-10 13:28:02 +0000

    Shannon front placket

--- designs/shannon/src/frontPlacket.mjs
+++ designs/shannon/src/frontPlacket.mjs
@@ -77,30 +77,159 @@ export const frontPlacket = {
       )
     }
 
-    paths.saRight = new Path()
-      .move(points.cfChest)
-      .line(points.frontPlacketCurveStart)
+    paths.saBase = new Path()
+      .move(points.frontPlacketCurveStart)
       .curve(
         points.frontPlacketCurveStartCp2,
         points.frontPlacketCurveEndCp1,
         points.frontPlacketCurveEnd
       )
-      .line(points.frontPlacketNeck)
+      .hide()
 
     paths.cfNeck = new Path()
       .move(points.hps)
       .curve(points.hpsCp2, points.cfNeckCp1, points.cfNeck)
       .split(points.frontPlacketNeck)[1]
+      .hide()
 
     paths.stitchingGuide = new Path()
       .move(points.neckOpening)
       .line(points.neckOpeningBottom)
       .curve(points.neckOpeningBottomCp2, points.cfNeckOpeningCp1, points.cfNeckOpening)
+      .hide()
+
+    macro('mirror', {
+      mirror: [points.cfNeck, points.cfChest],
+      paths: ['saBase', 'cfNeck', 'stitchingGuide'],
+      prefix: 'm',
+    })
+
+    paths.seam = paths.mSaBase
+      .reverse()
+      .line(points.frontPlacketCurveStart)
+      .join(paths.saBase)
+      .line(points.frontPlacketNeck)
+      .join(paths.cfNeck)
+      .join(paths.mCfNeck.reverse())
+      .line(paths.mSaBase.end())
+      .close()
 
     if (complete) {
-      // .attr('class', 'fabric help')
-      // .attr('data-text', 'Stitching - Line')
-      // .attr('data-text-class', 'center')
+      //grainline
+      points.grainlineFrom = points.cfNeckCp1.flipX(points.cfNeck)
+      points.grainlineTo = new Point(points.grainlineFrom.x, points.frontPlacketCurveEnd.y)
+      macro('grainline', {
+        from: points.grainlineFrom,
+        to: points.grainlineTo,
+      })
+      //notches
+      const flip = ['neckTieTop0', 'neckTieTop1', 'neckTieBottom0', 'neckTieBottom1']
+      for (const p of flip) points[p + 'F'] = points[p].flipX(points.cfNeck)
+      macro('sprinkle', {
+        snippet: 'notch',
+        on: ['neckTieTop0', 'neckTieTop1', 'neckTieTop0F', 'neckTieTop1F'],
+      })
+      macro('sprinkle', {
+        snippet: 'bnotch',
+        on: ['neckTieBottom0', 'neckTieBottom1', 'neckTieBottom0F', 'neckTieBottom1F'],
+      })
+      //title
+      points.title = new Point(points.cfNeckCp1.x / 2, points.cfChest.y / 2)
+      macro('title', {
+        at: points.title,
+        nr: '3',
+        title: 'Front Placket',
+        scale: 1 / 3,
+      })
+      //stitchingGuide
+      paths.stitchingGuide = paths.stitchingGuide
+        .join(paths.mStitchingGuide.reverse())
+        .attr('class', 'fabric help')
+        .attr('data-text', 'Stitching - Line')
+        .attr('data-text-class', 'center')
+
+      if (sa) {
+        const neckSa = sa * options.neckSaWidth * 100
+
+        if (options.frontPlacketCurve > 0.01) {
+          if (options.frontPlacketStyle == 'straight') {
+            points.saFrontPlacketCurveStart = utils.beamIntersectsY(
+              points.frontPlacketCurveStart
+                .shiftTowards(points.frontPlacketCurveStartCp2, sa)
+                .rotate(-90, points.frontPlacketCurveStart),
+              points.frontPlacketCurveStartCp2
+                .shiftTowards(points.frontPlacketCurveStart, sa)
+                .rotate(90, points.frontPlacketCurveStartCp2),
+              points.frontPlacketBottom.y + sa
+            )
+            points.saFrontPlacketCurveEnd = utils.beamIntersectsX(
+              points.frontPlacketCurveEndCp1
+                .shiftTowards(points.frontPlacketCurveEnd, sa)
+                .rotate(-90, points.frontPlacketCurveEndCp1),
+              points.frontPlacketCurveEnd
+                .shiftTowards(points.frontPlacketCurveEndCp1, sa)
+                .rotate(90, points.frontPlacketCurveEnd),
+              points.frontPlacketNeck.x + sa
+            )
+          } else {
+            points.saFrontPlacketCurveStart = paths.saBase.offset(sa).start()
+            points.saFrontPlacketCurveEnd = paths.saBase.offset(sa).end()
+          }
+        } else {
+          points.saFrontPlacketCurveStart = points.frontPlacketCurveStart.translate(sa, sa)
+          points.saFrontPlacketCurveEnd = points.saFrontPlacketCurveStart
+        }
+        points.saFrontPlacketNeck = new Point(
+          points.saFrontPlacketCurveEnd.x,
+          paths.cfNeck.offset(neckSa).start().y
+        )
+
+        if (options.frontPlacketCurve == 1) {
+          points.saCfChest = utils.beamIntersectsX(
+            points.frontPlacketCurveStart
+              .shiftTowards(points.frontPlacketCurveStartCp2, sa)
+              .rotate(-90, points.frontPlacketCurveStart),
+            points.frontPlacketCurveStartCp2
+              .shiftTowards(points.frontPlacketCurveStart, sa)
+              .rotate(90, points.frontPlacketCurveStartCp2),
+            points.cfChest.x
+          )
+        } else {
+          points.saCfChest = points.cfChest.shift(-90, sa)
+        }
+
+        const flip = ['saFrontPlacketCurveStart', 'saFrontPlacketCurveEnd', 'saFrontPlacketNeck']
+        for (const p of flip) points[p + 'F'] = points[p].flipX(points.cfNeck)
+
+        const drawSaBottom = () => {
+          if (options.frontPlacketCurve <= '0.01') {
+            return new Path()
+              .move(points.saFrontPlacketCurveEndF)
+              .line(points.saFrontPlacketCurveStartF)
+              .line(points.saCfChest)
+              .line(points.saFrontPlacketCurveStart)
+              .line(points.saFrontPlacketCurveEnd)
+          } else {
+            return new Path()
+              .move(points.saFrontPlacketCurveEndF)
+              .join(paths.mSaBase.reverse().offset(sa))
+              .line(points.saFrontPlacketCurveStartF)
+              .line(points.saCfChest)
+              .line(points.saFrontPlacketCurveStart)
+              .join(paths.saBase.offset(sa))
+              .line(points.saFrontPlacketCurveEnd)
+          }
+        }
+
+        paths.sa = drawSaBottom()
+          .line(points.saFrontPlacketNeck)
+          .join(paths.cfNeck.offset(neckSa))
+          .join(paths.mCfNeck.reverse().offset(neckSa))
+          .line(points.saFrontPlacketNeckF)
+          .line(points.saFrontPlacketCurveEndF)
+          .close()
+          .attr('class', 'fabric sa')
+      }
     }
 
     return part
