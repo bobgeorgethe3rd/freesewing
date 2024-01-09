31c7397acfd49e375fcba4569f85689aa2fc3b8c
bobgeorgethe3rd <bobgeorgethe3rd@googlemail.com>
2024-01-09 05:57:20 +0000

    Rose Sleeve

--- designs/rose/src/index.mjs
+++ designs/rose/src/index.mjs
@@ -7,12 +7,13 @@ import { front } from './front.mjs'
 import { sideFront } from './sideFront.mjs'
 import { back } from './back.mjs'
 import { sideBack } from './sideBack.mjs'
+import { sleeve } from './sleeve.mjs'
 
 // Create new design
 const Rose = new Design({
   data,
-  parts: [front, sideFront, back, sideBack],
+  parts: [front, sideFront, back, sideBack, sleeve],
 })
 
 // Named exports
-export { front, sideFront, back, sideBack, Rose }
+export { front, sideFront, back, sideBack, sleeve, Rose }
---
+++ designs/rose/src/sleeve.mjs
@@ -0,0 +1,63 @@
+import { sleeveBase } from '@freesewing/spreadsleeve'
+import { sleeve as capSleeve } from '@freesewing/capsleeve'
+import { back } from './back.mjs'
+
+export const sleeve = {
+  name: 'rose.sleeve',
+  from: sleeveBase,
+  after: back,
+  hide: {
+    from: true,
+  },
+  options: {
+    //Imported
+    ...capSleeve.options,
+    //Constants
+    useVoidStores: false, //Altered for Rose
+    sleeveFlounces: 'none', //Locked for Rose
+    sleeveBands: false, //Locked for Rose
+    //Sleeves
+    sleevesBool: { bool: true, menu: 'sleeves' },
+  },
+  draft: (sh) => {
+    //draft
+    const {
+      store,
+      sa,
+      Point,
+      points,
+      Path,
+      paths,
+      options,
+      complete,
+      paperless,
+      macro,
+      utils,
+      measurements,
+      part,
+      snippets,
+      Snippet,
+      absoluteOptions,
+      log,
+    } = sh
+    //draft
+    if (options.sleevesBool) {
+      capSleeve.draft(sh)
+    } else {
+      part.hide()
+      return part
+    }
+
+    if (complete) {
+      //title
+      macro('title', {
+        at: points.title,
+        nr: '3',
+        title: 'Sleeve (' + utils.capitalize(options.spreadType) + ' Spread)',
+        scale: 0.4,
+      })
+    }
+
+    return part
+  },
+}
