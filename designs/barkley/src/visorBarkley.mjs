4a8b7b93e34f3d19dd7e7e621e61e60d84a7160f
bobgeorgethe3rd <bobgeorgethe3rd@googlemail.com>
2024-02-07 14:23:59 +0000

    Nathaniel Setup

--- designs/barkley/src/visorBarkley.mjs
+++ designs/barkley/src/visorBarkley.mjs
@@ -57,12 +57,20 @@ export const visorBarkley = {
             .start()
             .shift(points.topRightCp1.angle(points.topRight) - 90, 1)
         )
+        points.saHemBaseStart = utils.beamIntersectsY(
+          points.topRight.shiftTowards(points.topRightCp2, crownSa).rotate(-90, points.topRight),
+          points.topRightCp2.shiftTowards(points.topRight, crownSa).rotate(90, points.topRightCp2),
+          points.saTopRight.y
+        )
 
         points.saTopLeft = points.saTopRight.flipX(points.topMid)
+        points.saHemBaseEnd = points.saHemBaseStart.flipX(points.topMid)
 
         paths.sa = new Path()
           .move(points.saTopRight)
+          .line(points.saHemBaseStart)
           .join(paths.hemBase.offset(crownSa))
+          .line(points.saHemBaseEnd)
           .line(points.saTopLeft)
           .join(paths.saBase.offset(sa))
           .line(points.saTopRight)
