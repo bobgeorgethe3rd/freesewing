078eeeda0fee987bcd19317dd7b066b5aa6718d1
bobgeorgethe3rd <bobgeorgethe3rd@googlemail.com>
2025-02-09 19:55:05 +0000

    add(frankie) frontBase.mjs

--- designs/frankie/src/index.mjs
+++ designs/frankie/src/index.mjs
@@ -5,12 +5,13 @@ import { i18n } from '../i18n/index.mjs'
 import { data } from '../data.mjs'
 // Parts
 import { back } from './back.mjs'
+import { frontBase } from './frontBase.mjs'
 
 // Create new design
 const Frankie = new Design({
   data,
-  parts: [back],
+  parts: [back, frontBase],
 })
 
 // Named exports
-export { back, i18n, Frankie }
+export { back, frontBase, i18n, Frankie }
