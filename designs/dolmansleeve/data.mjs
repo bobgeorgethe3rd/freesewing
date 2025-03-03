7240f9bd8519207376f2c192a7ce58a76ea0506b
bobgeorgethe3rd <bobgeorgethe3rd@googlemail.com>
2025-03-03 18:03:26 +0000

    Gather head sleeve setup

--- designs/dolmansleeve/data.mjs
+++ designs/dolmansleeve/data.mjs
@@ -1,5 +1,5 @@
 // This file is auto-generated | All changes you make will be overwritten.
 export const name = '@freesewing/dolmansleeve'
-export const number = ''
+export const number = 'C0018'
 export const version = '3.0.0-alpha.8'
 export const data = { name, number, version }
