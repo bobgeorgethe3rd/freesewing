5d062b83ab5fa0278992506a0ec47d9b5112174f
bobgeorgethe3rd <bobgeorgethe3rd@googlemail.com>
2025-04-02 23:10:35 +0100

    missing(lab) missing pages for new categories

---
+++ sites/lab/pages/hats/index.js
@@ -0,0 +1,14 @@
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import { PatternListPageTemplate } from 'site/page-templates/design-list.mjs'
+
+const Page = () => <PatternListPageTemplate section="hats" />
+
+export default Page
+
+export async function getStaticProps({ locale }) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale)),
+    },
+  }
+}
