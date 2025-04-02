ad7398b1718a96e0d440ffa4143f2f7d09f64138
bobgeorgethe3rd <bobgeorgethe3rd@googlemail.com>
2025-04-02 02:13:58 +0000

    add(frankie) about.json

--- designs/frankie/data.mjs
+++ designs/frankie/data.mjs
@@ -1,4 +1,4 @@
 // This file is auto-generated | All changes you make will be overwritten.
 export const name = '@freesewing/frankie'
-export const version = '3.3.0-rc.1'
+export const version = '4.0.0'
 export const data = { name, version }
