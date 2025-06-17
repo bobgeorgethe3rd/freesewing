c725ff208ec6b3aab0f8aff0ecb8032a9903ed5e
bobgeorgethe3rd <bobgeorgethe3rd@googlemail.com>
2025-06-17 14:02:07 +0100

    fix(antiope) pocket insert seam allowance

--- designs/antiope/src/pocket.mjs
+++ designs/antiope/src/pocket.mjs
@@ -1,9 +1,9 @@
 import { pocket as inseamPocket } from '@freesewing/inseampocket'
-import { base } from '@freesewing/sarah'
+import { back } from '@freesewing/sarah'
 
 export const pocket = {
   name: 'antiope.pocket',
-  after: base,
+  after: back,
   options: {
     //Imported
     ...inseamPocket.options,
@@ -15,10 +15,18 @@ export const pocket = {
   },
   measurements: [...inseamPocket.measurements],
   draft: (sh) => {
-    const { macro, points, measurements, options, store, complete, part, log } = sh
+    const { macro, points, measurements, options, store, sa, complete, part, log } = sh
     //set stores
     store.set('anchorSeamLength', store.get('styleWaistFront') * 0.5)
     store.set('insertSeamLength', measurements.waistToFloor)
+    if (sa) {
+      store.set(
+        'insertSeamSa',
+        options.closurePosition == 'side'
+          ? sa * options.closureSaWidth * 100
+          : sa * options.sideSeamSaWidth * 100
+      )
+    }
     //set Render stroke Draft
     if (options.pocketsBool) {
       inseamPocket.draft(sh)
