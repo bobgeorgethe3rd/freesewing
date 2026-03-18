afaeb3ff16eb6c3f2163bcaa51449fe26a1b1f7c
bobgeorgethe3rd <bobgeorgethe3rd@googlemail.com>
2026-03-18 00:22:58 +0000

    Darren setup

---
+++ designs/darren/tests/shared.test.mjs
@@ -0,0 +1,16 @@
+// This file is auto-generated | Any changes you make will be overwritten.
+import { Darren } from '../src/index.mjs'
+
+// Shared tests
+import { testPatternConfig } from '../../../tests/designs/config.mjs'
+import { testPatternDrafting } from '../../../tests/designs/drafting.mjs'
+import { testPatternSampling } from '../../../tests/designs/sampling.mjs'
+
+// Test config
+testPatternConfig(Darren)
+
+// Test drafting - Change the second parameter to `true` to log errors
+testPatternDrafting(Darren, false)
+
+// Test sampling - Change the second parameter to `true` to log errors
+testPatternSampling(Darren, false)
