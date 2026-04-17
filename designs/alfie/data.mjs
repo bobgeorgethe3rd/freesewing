c65780559ee7bf25461eecd55678787e9c680d70
bobgeorgethe3rd <bobgeorgethe3rd@googlemail.com>
2026-04-17 12:06:12 +0100

    Alfie setup

---
+++ designs/alfie/data.mjs
@@ -0,0 +1,5 @@
+// This file is auto-generated | All changes you make will be overwritten.
+export const name = '@freesewing/alfie'
+export const number = '0059'
+export const version = '3.0.0-alpha.8'
+export const data = { name, number, version }
