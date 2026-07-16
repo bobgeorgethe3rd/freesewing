57da7e76dea0c0c467b06ce476a9be55a29ac527
bobgeorgethe3rd <bobgeorgethe3rd@googlemail.com>
2026-07-16 09:20:00 +0100

    Combined Shirt Collar setup

---
+++ designs/combinedshirtcollar/markdown/en.md
@@ -0,0 +1,12 @@
+
+Combinedshirtcollar is a component design and is intended to be imported and use by across multiple designs.
+It is **not** designed to be used standalone and should not be printed off by itself.
+
+---
+## How to import Combinedshirtcollar
+---
+
+To import Combinedshirtcollar you.....
+
+<fixMe Add how to import Combinedshirtcollar design>
+
---
+++ designs/combinedshirtcollar/tests/shared.test.mjs
@@ -0,0 +1,16 @@
+// This file is auto-generated | Any changes you make will be overwritten.
+import { Combinedshirtcollar } from '../src/index.mjs'
+
+// Shared tests
+import { testPatternConfig } from '../../../tests/designs/config.mjs'
+import { testPatternDrafting } from '../../../tests/designs/drafting.mjs'
+import { testPatternSampling } from '../../../tests/designs/sampling.mjs'
+
+// Test config
+testPatternConfig(Combinedshirtcollar)
+
+// Test drafting - Change the second parameter to `true` to log errors
+testPatternDrafting(Combinedshirtcollar, false)
+
+// Test sampling - Change the second parameter to `true` to log errors
+testPatternSampling(Combinedshirtcollar, false)
